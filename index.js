0a15682b788bbe4680b849476217a1656423253d
ablydevin <136481955+ablydevin@users.noreply.github.com>
2024-01-16 09:39:48 -0500

    Adding table formatted output

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const { program } = require("commander");
-var EventSource = require('eventsource')
+const columnify = require('columnify')
+const EventSource = require("eventsource");
+require('dotenv').config()
+//console.log(process.env)
+
+defaultColumnOptions = {
+  columnSplitter: ' | '
+}
 
 const STREAMING_DELAY_IN_SECONDS = 1;
+const ABLY_API_KEY = process.env.ABLY_API_KEY;
 
 const sleep = (ms) => {
   //console.log(`sleeping`)
@@ -9,10 +17,9 @@ const sleep = (ms) => {
 };
 
 program
-  .name('ablycli')
+  .name("ablycli")
   .version("1.0.0")
   .description("A simple CLI application built with Node.js");
-
 
 program
   .command("greet <name>")
@@ -24,26 +31,37 @@ program
 program
   //how do I add optional parameters like channel
   .command("tail")
-  .argument('<appid>')
-  .option('-c, --channel <name>', 'name of the channel to watch')
+  .argument("<appid>")
+  .option("-c, --channel <name>", "name of the channel to watch")
   .description("Watch application events in real time")
-  .action(async (app) =>  {
-    console.log('Starting Watch')
+  .action(async (app) => {
+
+    defaultColumnOptions.columns = ['name','data','transport']
+    console.log(columnify([], defaultColumnOptions))
+
     //app connection events
-    var connections = new EventSource(`https://realtime.ably.io/sse?v=1.2&channels=[app:${app}:meta]connection.lifecycle&key=`)
-    connections.addEventListener('message', function (e) {
-      console.log(e.data)
-    })
+    var connections = new EventSource(
+      `https://realtime.ably.io/sse?v=1.2&channels=[app:${app}:meta]connection.lifecycle&key=${ABLY_API_KEY}`
+    );
+    connections.addEventListener("message", function (e) {
+      let data = JSON.parse(e.data);
+      defaultColumnOptions.showHeaders = false;
+      defaultColumnOptions.truncate = true;
+      console.log(columnify([data], defaultColumnOptions))
+      //console.log(e.data);
+      //console.log(table.toString())
+    });
     connections.onerror = function (err) {
       if (err) {
         if (err.status === 401 || err.status === 403) {
-          console.log('not authorized');
+          console.log("not authorized");
         }
       }
     };
 
-    //channel 
-    var channels = new EventSource(`https://realtime.ably.io/sse?v=1.2&channels=[app:${app}:meta]channel.lifecycle&key=`)
+
+    //channel
+    var channels = new EventSource(`https://realtime.ably.io/sse?v=1.2&channels=[app:${app}:meta]channel.lifecycle&key=${ABLY_API_KEY}`)
     channels.addEventListener('message', function (e) {
      console.log(e.data)
     })
@@ -61,5 +79,3 @@ program
   });
 
 program.parse(process.argv);
-
-
